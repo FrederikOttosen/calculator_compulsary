60d8fe82cf9336ca558b26b3289a6e9804bae7a0
FrederikOtto <frederik.skarbyottosen@gmail.com>
2023-10-10 08:54:11 +0200

    More changes

--- addition/Program.cs
+++ addition/Program.cs
@@ -1,12 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers(); // <-- Make sure you have this line
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAuthorization();
 
-// Configure CORS
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
@@ -19,7 +17,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
